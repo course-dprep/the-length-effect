2c8e2c6fde1a6100077411a7ab29135528002e79
borisova-simona <s.borisova@tilburguniversity.com>
2025-10-09 17:24:52 +0200

    Fix Makefile paths and improve clean command

--- makefile
+++ makefile
@@ -1,10 +1,10 @@
-R      := Rscript --vanilla
-MAKE   := make
-DP     := src/data-preparation
-AN     := src/analysis
-DATA   := data
-TEMP   := gen/temp
-OUT    := gen/output
+DP	:= $(ROOT)/src/data-preparation
+AN	:= $(ROOT)/src/analysis
+DATA	:= $(ROOT)/data
+GEN	:= := $(ROOT)/gen
+TEMP	:= $(ROOT)/gen/temp
+OUT	:= $(ROOT)/gen/output
+PAPER	:= $(ROOT)/paper/output
 
 
 .PHONY: all data-preparation analysis report clean help
@@ -16,3 +16,8 @@ data-preparation:  ## Run data-preparation pipeline
 
 analysis: data-preparation  ## Run analysis after data-prep
 	$(MAKE) -C $(AN)
+	
+clean:
+	R -e "unlink('DATA',  recursive=TRUE, force=TRUE)"
+	R -e "unlink('GEN',  recursive=TRUE, force=TRUE)"
+	R -e "unlink('PAPER', recursive=TRUE, force=TRUE)"
--- src/analysis/lm_main_effect.R
+++ src/analysis/lm_main_effect.R
@@ -8,11 +8,8 @@ library(gt)
 library(tibble)   
 library(knitr)     
 library(kableExtra)
-install.packages(c("modelsummary", "sandwich", "lmtest", "broom"))
 library(modelsummary)
 library(sandwich)
-install.packages("contrib.url")
-install.packages("ggeffects")
 library(ggeffects)
 
 
@@ -33,24 +30,22 @@ p1 <- ggplot(movies_final_clean, aes(x = runtime_minutes, y = average_rating)) +
   geom_smooth(method = "lm", color = "#1A5276", se = FALSE, size = 1.2) +
   labs(
     title = "Relationship between Runtime and IMDb Rating",
-    subtitle = "Controlling for release year",
     x = "Runtime (minutes)",
     y = "Average IMDb Rating") +
   theme_minimal(base_size = 13)
+
 ggsave(
-  filename = "Final_result_lm.pdf",
+  filename = file.path(out_dir, "Final_result_lm.png"),
   plot     = p1,
-  path     = out_dir,
-  width    = 7,
-  height   = 4,
-  units    = "in",
-  device   = "pdf"
+  width = 7, height = 4, units = "in",  bg = "white",
+  dpi = 150
 )
+
 # Predicted relationship (controlling for release year)
 pred <- ggpredict(model_1, terms = "runtime_minutes [all]")
 
-ggplot(pred, aes(x = x, y = predicted)) +
-  geom_line(color = "#1A5276", size = 1.2) +
+p_pred <- ggplot(pred, aes(x = x, y = predicted)) +
+  geom_line(color = "#1A5276", linewidth = 1.2) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), fill = "#5DADE2", alpha = 0.2) +
   labs(
     title = "Predicted IMDb Rating vs Runtime",
@@ -59,7 +54,12 @@ ggplot(pred, aes(x = x, y = predicted)) +
     y = "Predicted IMDb Rating"
   ) +
   theme_minimal(base_size = 13)
-ggsave(file.path(out_dir, "Predicted_model.pdf"), p1, width = 7, height = 4, dpi = 180)
+
+ggsave(
+  filename = file.path(out_dir, "Predicted_model.pdf"),
+  plot = p_pred, 
+  width = 7, height = 4, units = "in", device = "pdf"
+  )
 
 
 # Quadratic Model 
@@ -68,10 +68,10 @@ model_quad <- lm(average_rating ~ runtime_minutes + I(runtime_minutes^2) + start
 summary(model_quad)
 
 
-p2 <- ggplot(movies_final_clean, aes(x = runtime_minutes, y = average_rating)) +
+p_quad <- ggplot(movies_final_clean, aes(x = runtime_minutes, y = average_rating)) +
   geom_point(alpha = 0.2, color = "#AED6F1") +
-  geom_smooth(method = "lm", formula = y ~ x, color = "#1A5276", se = FALSE, size = 1.1) +
-  geom_smooth(method = "lm", formula = y ~ poly(x, 2), color = "#E74C3C", se = FALSE, size = 1.1) +
+  geom_smooth(method = "lm", formula = y ~ x, color = "#1A5276", se = FALSE, linewidth  = 1.1) +
+  geom_smooth(method = "lm", formula = y ~ poly(x, 2), color = "#E74C3C", se = FALSE, linewidth  = 1.1) +
   coord_cartesian(xlim = c(40, 250), ylim = c(0, 10)) +
   labs(
     title = "Linear vs Quadratic Fit: Runtime and IMDb Rating",
@@ -79,14 +79,12 @@ p2 <- ggplot(movies_final_clean, aes(x = runtime_minutes, y = average_rating)) +
     y = "Average IMDb Rating",
     caption = "Blue = linear model; Red = quadratic model (within observed data range)") +
   theme_minimal(base_size =13)
+
 ggsave(
-  filename = "Linear_vs_Quadratic_Fit.pdf",
-  plot     = p2,
-  path     = out_dir,
-  width    = 7,
-  height   = 4,
-  units    = "in",
-  device   = "pdf"
+  filename = file.path(out_dir, "Linear_vs_Quadratic_Fit.png"),
+  plot     = p_quad,
+  width = 7, height = 4, units = "in",  bg = "white",
+  dpi = 150
 )
 
 #Robustness Check - interaction with release year
