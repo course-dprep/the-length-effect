71b9185a8d5aac2bcf8e951f3a94ea23b823b299
AgataKopiczynska <a.kopiczynska@tilburguniversity.edu>
2025-10-10 20:10:09 +0200

    Fixing tables and removing R scripts

--- src/data-preparation/setup.R
+++
@@ -1,9 +0,0 @@
-# Set up
-knitr::opts_chunk$set(
-  echo    = FALSE,
-  message = FALSE,
-  warning = FALSE,
-  fig.align = "center",
-  fig.width = 7, fig.height = 4, dpi = 180
-)
-options(tidyverse.quiet = TRUE)
